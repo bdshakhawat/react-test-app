67b31cf12e1cd1af052918b42bca2db533be8e39
bdshakhawat <mdshakwat.hossain@gmail.com>
2020-08-21 23:12:17 -0700

    firs commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var man = {
+        name: 'sumon',
+       job : 'Teacher',
+       institute : 'MCPSC'
+  }
+  var style = {
+    color : 'red',
+    backgroundColor:'cyan'
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -10,10 +19,17 @@ function App() {
         <p>
           Edit done <code>src/App.js</code> and save to reload.
         </p>
-        
+  <h2 className="" style = {style}>My name is  {man.name +" ." +" "+ 'I am a'+" "+ man.job +" "+ 'of'+" "+ man.institute}</h2>
+        <p>My first paragraph</p>
+        <Person></Person>
+        <Person></Person>
+        <Person></Person>
       </header>
     </div>
   );
+}
+function Person(){
+  return <h1>Sakib Al HasAN</h1>
 }
 
 export default App;
