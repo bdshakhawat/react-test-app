5acc414245d4f73a69b12715c96c039ae9ad00d4
bdshakhawat <mdshakwat.hossain@gmail.com>
2020-08-22 00:06:09 -0700

    second commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
   var man = {
         name: 'sumon',
        job : 'Teacher',
@@ -17,19 +18,29 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit done <code>src/App.js</code> and save to reload.
+          
         </p>
   <h2 className="" style = {style}>My name is  {man.name +" ." +" "+ 'I am a'+" "+ man.job +" "+ 'of'+" "+ man.institute}</h2>
-        <p>My first paragraph</p>
-        <Person></Person>
-        <Person></Person>
+        <p>My first React Element</p>
+        <Person name = "Alomgir" ></Person>
+        <Person name= "Raj Razzak" Heroin = "Rojina" ></Person>
         <Person></Person>
       </header>
     </div>
   );
 }
-function Person(){
-  return <h1>Sakib Al HasAN</h1>
+function Person(props){
+  const personStyle= {
+    border:'2px solid red',
+    margin:'10px'
+
+  }
+  return ( 
+    <div className="" style= {personStyle} >;
+      <h1>{props.name +" " +" "+ "Heroin"+" "+props.Heroin}</h1>
+      <h3>Player of the year</h3>
+    </div>
+  )
 }
 
 export default App;
